b10ad1e4f698e9c2b251fe54e0fd5e81757c272c
Hamid Rafiyan <44866201+persia67@users.noreply.github.com>
2026-09-18 08:05:27 +0330

    chore: bump application version to 4.15.11

--- src/constants.ts
+++ src/constants.ts
@@ -2,7 +2,7 @@ import { Violation, Severity, User, AppSettings, Reward, CodeItem, N8nConfig } f
 
 export const DEFAULT_COMPANY_LOGO = '/logo.png';
 
-export const APP_VERSION = "4.15.9";
+export const APP_VERSION = "4.15.11";
 
 export const TRANSLATIONS = {
   fa: {
